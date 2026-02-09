386361fbc731bb8ef0c8beded44cb11954495e6a
nguyenductai1983 <nguyenductai1983@gmail.com>
2026-02-09 16:56:39 +0700

    yy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // 1. Chạy Seeder Phòng ban trước (vì User cần Department ID)
-        $this->call(DepartmentSeeder::class);
+        // $this->call(DepartmentSeeder::class);
 
         // 2. Chạy Seeder Quyền và Vai trò
-        $this->call(RolesAndPermissionsSeeder::class);
+        // $this->call(RolesAndPermissionsSeeder::class);
 
         // 3. Tạo User Admin mẫu (Sau khi đã có Role và Department)
         // Kiểm tra xem user đã tồn tại chưa để tránh lỗi trùng email khi chạy lại
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Admin',
                 'password' => Hash::make('12345678'), // Đặt mật khẩu
-                'department_id' => 1, // Gán vào phòng IT (ID 1) nếu muốn
+                // 'department_id' => 1, // Gán vào phòng IT (ID 1) nếu muốn
             ]
         );
 
